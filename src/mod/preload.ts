0817855d545ca4b22d6e0bf8ea8faf5bc38d3100
nsvl <companiumpidors@gmail.com>
2026-05-23 20:54:59 +0300

    Fix preload: use global electron instead of require()

--- src/mod/preload.ts
+++ src/mod/preload.ts
@@ -1,23 +1,39 @@
-const { contextBridge, ipcRenderer, globalShortcut, BrowserWindow } = require("electron");
+declare const electron: {
+  contextBridge: {
+    exposeInMainWorld(key: string, api: Record<string, unknown>): void;
+  };
+  ipcRenderer: {
+    invoke(channel: string, ...args: unknown[]): Promise<unknown>;
+    send(channel: string, ...args: unknown[]): void;
+    on(channel: string, listener: (event: unknown, ...args: unknown[]) => void): void;
+    removeListener(channel: string, listener: (event: unknown, ...args: unknown[]) => void): void;
+  };
+  globalShortcut: {
+    register(accelerator: string, callback: () => void): void;
+  };
+  BrowserWindow: {
+    getFocusedWindow(): { webContents: { toggleDevTools(): void } } | null;
+  };
+};
 
-contextBridge.exposeInMainWorld("yandexMusicMod", {
-  getStorageValue: (key: string) => ipcRenderer.invoke("yandexMusicMod.getStorageValue", key),
-  setStorageValue: (key: string, value: unknown) => ipcRenderer.send("yandexMusicMod.setStorageValue", key, value),
+electron.contextBridge.exposeInMainWorld("yandexMusicMod", {
+  getStorageValue: (key: string) => electron.ipcRenderer.invoke("yandexMusicMod.getStorageValue", key),
+  setStorageValue: (key: string, value: unknown) => electron.ipcRenderer.send("yandexMusicMod.setStorageValue", key, value),
   onStorageChanged: (cb: (key: string, value: unknown) => void) => {
-    const listener = (_e: Electron.IpcRendererEvent, key: string, value: unknown) => cb(key, value);
-    ipcRenderer.on("yandexMusicMod.storageValueUpdated", listener);
-    return () => ipcRenderer.removeListener("yandexMusicMod.storageValueUpdated", listener);
+    const listener = (_e: unknown, key: string, value: unknown) => cb(key, value);
+    electron.ipcRenderer.on("yandexMusicMod.storageValueUpdated", listener);
+    return () => electron.ipcRenderer.removeListener("yandexMusicMod.storageValueUpdated", listener);
   },
   downloadTrack: (downloadInfo: DownloadInfo, trackMeta: TrackMeta, customDownloadPath?: string) =>
-    ipcRenderer.invoke("yandexMusicMod.downloadTrack", downloadInfo, trackMeta, customDownloadPath),
-  openDownloadDirectory: () => ipcRenderer.send("yandexMusicMod.openDownloadDirectory"),
-  selectDownloadFolder: () => ipcRenderer.invoke("yandexMusicMod.selectDownloadFolder"),
-  openFolder: (folderPath: string) => ipcRenderer.invoke("yandexMusicMod.openFolder", folderPath),
-  axios: (config: Record<string, unknown>) => ipcRenderer.invoke("yandexMusicMod.axios", config),
+    electron.ipcRenderer.invoke("yandexMusicMod.downloadTrack", downloadInfo, trackMeta, customDownloadPath),
+  openDownloadDirectory: () => electron.ipcRenderer.send("yandexMusicMod.openDownloadDirectory"),
+  selectDownloadFolder: () => electron.ipcRenderer.invoke("yandexMusicMod.selectDownloadFolder"),
+  openFolder: (folderPath: string) => electron.ipcRenderer.invoke("yandexMusicMod.openFolder", folderPath),
+  axios: (config: Record<string, unknown>) => electron.ipcRenderer.invoke("yandexMusicMod.axios", config),
 });
 
-globalShortcut.register("CommandOrControl+Shift+I", () => {
-  const focusedWindow = BrowserWindow.getFocusedWindow();
+electron.globalShortcut.register("CommandOrControl+Shift+I", () => {
+  const focusedWindow = electron.BrowserWindow.getFocusedWindow();
   if (focusedWindow) {
     focusedWindow.webContents.toggleDevTools();
   }
